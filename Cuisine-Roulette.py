fc1f738fc7b74fae65cc041bd55253e4e1781024
sk8378 <84359100+sk8378@users.noreply.github.com>
2021-06-22 21:02:07 -0400

    Cuisine Inputs

--- Cuisine-Roulette.py
+++ Cuisine-Roulette.py
@@ -56,8 +56,28 @@ HOST = 'https://api.yelp.com'
 SEARCH_PATH = '/v3/businesses/search'
 BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
 
+Cuisine_Names = []
+while True:
+    Cuisine_Name = input("Please input a Cuisine type, or 'DONE' if your whole party has voted: ")
+    if Cuisine_Name == "DONE":
+        break
+    else:
+        Cuisine_Names.append(Cuisine_Name)
 
-Cuisine_Name = input("Please input a Cuisine type: ")
+
+#for Cuisine_Name1 in Cuisine_Names:
+#   matching_cuisine = [p for p in Cuisine_Names]
+#  matching_cuisine = matching_cuisine[0]
+#    Selected_Cuisine.append(matching_cuisine)
+
+
+
+
+valid_options = Cuisine_Names
+random_choice = random.choice(valid_options)
+
+
+Cuisine_Name = random_choice
 Address = input("Please input your address: ")
 SEARCH_LIMIT = 3
 
